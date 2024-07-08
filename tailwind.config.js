79c05e142d596be6c4197c500de75fb5650483e8
Matias D. Sanchez R <matiasdsanchezr@gmail.com>
2024-07-08 17:28:29 -0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
       colors: {
         primary: "var(--primary-color)",
       },
+      keyframes: {
+        zoom: {
+          "0%, 100%": { transform: "scale(1.1)" },
+          "50%": { transform: "scale(1);" },
+        },
+        translate: {
+          "0%, 100%": { objectPosition: "center top" },
+          "50%": { objectPosition: "center center" },
+        },
+      },
+      animation: {
+        zoom: "zoom 5s ease-in-out infinite",
+        translate: "translate 15s ease-in-out infinite",
+      },
     },
   },
   plugins: [],
